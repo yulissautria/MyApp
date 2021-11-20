727d4c10528de5277dec11fe717f942c78dc8020
yulissa <utriayulissa@gmail.com>
2021-11-19 19:13:13 -0500

    First commit

---
+++ app/src/main/java/com/example/myapp/WelcomeActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+
+class WelcomeActivity : AppCompatActivity() {
+    private lateinit var txvUsername: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+       /* setSupportActionBar(findViewById(R.id.my_toolbar)) */
+        txvUsername = findViewById(R.id.txvUsername)
+        txvUsername.setText(intent.getStringExtra("username"))
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Element added", Snackbar.LENGTH_LONG). show()
+
+        }
+    }
+}
